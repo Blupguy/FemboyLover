4b0a45e096fa65725fb018eb8a4b5f9e6f188895
Blupguy <joedunken279@gmail.com>
2022-09-12 00:32:49 -0800

    idk honestly
    i was messing around with some stuff and deleted most of it im just here to commit my no changes to main lmao

---
+++ WebsiteStuff/Javascript.js
@@ -0,0 +1,8 @@
+let userQuestion = "Test 8ball here!";
+
+var answer = ["Yes", "No", "Ask Again", "Certainly Not", "Definitely!", "Try again", "Ask Once More", "The Almighty Ruler of the land, Binxie Boy, has pondered upon this question once before, he truly believes you can find your own answer, now go forth and find your destiny!"];
+
+window.onload = function(){
+  var eight = document.getElementById("eight");
+  var answer = document.getElementById("answer");
+}
